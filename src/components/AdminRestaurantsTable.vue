89221435b29f03288f08eb36135cf5f68042430f
Sean <siang720@gmail.com>
2020-02-28 09:29:17 +0800

    change <a> to <router-link>

--- src/components/AdminRestaurantsTable.vue
+++ src/components/AdminRestaurantsTable.vue
@@ -31,7 +31,15 @@
             Show
           </router-link>
 
-          <a href="#" class="btn btn-link">Edit</a>
+          <router-link
+            :to="{
+              name: 'admin-restaurant-edit',
+              params: { id: restaurant.id }
+            }"
+            class="btn btn-link"
+          >
+            Edit
+          </router-link>
 
           <button
             type="button"
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -17,11 +17,23 @@
     <div id="navbarSupportedContent" class="navbar-collapse collapse">
       <div class="ml-auto d-flex align-items-center">
         <!-- is user is admin -->
-        <router-link v-if="currentUser.isAdmin" to="#" class="text-white mr-3">管理員後台</router-link>
+        <router-link
+          v-if="currentUser.isAdmin"
+          to="/admin"
+          class="text-white mr-3"
+          >管理員後台</router-link
+        >
         <!-- is user is login -->
         <template v-if="isAuthenticated">
-          <router-link to="#" class="text-white mr-3">{{ currentUser.name || '使用者' }} 您好</router-link>
-          <button type="button" class="btn btn-sm btn-outline-success my-2 my-sm-0">登出</button>
+          <router-link to="#" class="text-white mr-3"
+            >{{ currentUser.name || "使用者" }} 您好</router-link
+          >
+          <button
+            type="button"
+            class="btn btn-sm btn-outline-success my-2 my-sm-0"
+          >
+            登出
+          </button>
         </template>
       </div>
     </div>
