2cc9ee5e8662b02823adcdcd7c8747b708ce5163
Sean <siang720@gmail.com>
2020-02-28 09:08:58 +0800

    add admin edit restaurant feature

--- src/components/AdminRestaurantForm.vue
+++ src/components/AdminRestaurantForm.vue
@@ -137,6 +137,20 @@ const dummyData = {
 };
 
 export default {
+  props: {
+    initialRestaurant: {
+      type: Object,
+      default: () => ({
+        name: "",
+        categoryId: "",
+        tel: "",
+        address: "",
+        description: "",
+        image: "",
+        openingHours: ""
+      })
+    }
+  },
   data() {
     return {
       restaurant: {
@@ -153,6 +167,10 @@ export default {
   },
   created() {
     this.fetchCategories();
+    this.restaurant = {
+      ...this.restaurant,
+      ...this.initialRestaurant
+    };
   },
   methods: {
     fetchCategories() {
