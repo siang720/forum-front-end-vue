82a1c50857f03e77afdd181685eaaf84eb8611b0
Sean <siang720@gmail.com>
2020-02-28 08:52:45 +0800

    add new restaurant form submit

---
+++ src/components/AdminRestaurantForm.vue
@@ -0,0 +1,175 @@
+<template>
+  <form @submit.stop.prevent="handleSubmit">
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input
+        id="name"
+        type="text"
+        v-model="restaurant.name"
+        class="form-control"
+        name="name"
+        placeholder="Enter name"
+        required
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="categoryId">Category</label>
+      <select
+        v-model="restaurant.categoryId"
+        id="categoryId"
+        class="form-control"
+        name="categoryId"
+        required
+      >
+        <option value="" selected disabled>
+          --請選擇--
+        </option>
+        <option
+          v-for="category in categories"
+          :key="category.id"
+          :value="category.id"
+        >
+          {{ category.name }}
+        </option>
+      </select>
+    </div>
+
+    <div class="form-group">
+      <label for="tel">Tel</label>
+      <input
+        id="tel"
+        v-model="restaurant.tel"
+        type="text"
+        class="form-control"
+        name="tel"
+        placeholder="Enter telephone number"
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="address">Address</label>
+      <input
+        id="address"
+        type="text"
+        v-model="restaurant.address"
+        class="form-control"
+        placeholder="Enter address"
+        name="address"
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="opening-hours">Opening Hours</label>
+      <input
+        v-model="restaurant.openingHours"
+        id="opening-hours"
+        type="time"
+        class="form-control"
+        name="opening_hours"
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="description">Description</label>
+      <textarea
+        id="description"
+        class="form-control"
+        v-model="restaurant.description"
+        rows="3"
+        name="description"
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="image">Image</label>
+      <img
+        v-if="restaurant.image"
+        :src="restaurant.image"
+        class="d-block img-thumbnail mb-3"
+        width="200"
+        height="200"
+      />
+      <input
+        id="image"
+        type="file"
+        name="image"
+        accept="image/*"
+        class="form-control-file"
+        @change="handleFileChange"
+      />
+    </div>
+
+    <button type="submit" class="btn btn-primary">
+      送出
+    </button>
+  </form>
+</template>
+
+<script>
+const dummyData = {
+  categories: [
+    {
+      id: 1,
+      name: "中式料理",
+      createdAt: "2019-06-22T09:00:43.000Z",
+      updatedAt: "2019-06-22T09:00:43.000Z"
+    },
+    {
+      id: 2,
+      name: "日本料理",
+      createdAt: "2019-06-22T09:00:43.000Z",
+      updatedAt: "2019-06-22T09:00:43.000Z"
+    },
+    {
+      id: 3,
+      name: "義大利料理",
+      createdAt: "2019-06-22T09:00:43.000Z",
+      updatedAt: "2019-06-22T09:00:43.000Z"
+    },
+    {
+      id: 4,
+      name: "墨西哥料理",
+      createdAt: "2019-06-22T09:00:43.000Z",
+      updatedAt: "2019-06-22T09:00:43.000Z"
+    }
+  ]
+};
+
+export default {
+  data() {
+    return {
+      restaurant: {
+        name: "",
+        catagoryId: "",
+        tel: "",
+        address: "",
+        description: "",
+        image: "",
+        openingHours: ""
+      },
+      categories: []
+    };
+  },
+  created() {
+    this.fetchCategories();
+  },
+  methods: {
+    fetchCategories() {
+      this.categories = dummyData.categories;
+    },
+    handleFileChange(e) {
+      const files = e.target.files;
+      if (!files.length) return; // 如果沒有檔案則離開此函式
+      // 否則產生預覽圖...
+      const imageURL = window.URL.createObjectURL(files[0]);
+      this.restaurant.image = imageURL;
+    },
+    handleSubmit(e) {
+      const form = e.target; // <form></form>
+      const formData = new FormData(form);
+      this.$emit("after-submit", formData);
+    }
+  }
+};
+</script>
