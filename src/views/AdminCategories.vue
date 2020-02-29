42c8f791e3adc361cdd3127797ebd8331cec81f4
Sean <siang720@gmail.com>
2020-02-29 14:56:22 +0800

    finish edit restaurant category name

--- src/views/AdminCategories.vue
+++ src/views/AdminCategories.vue
@@ -44,13 +44,41 @@
             {{ category.id }}
           </th>
           <td class="position-relative">
-            <div class="category-name">
+            <div v-show="!category.isEditing" class="category-name">
               {{ category.name }}
             </div>
+            <input
+              v-show="category.isEditing"
+              v-model="category.name"
+              type="text"
+              class="form-control"
+            />
+            <span
+              v-show="category.isEditing"
+              class="cancel"
+              @click="handleCancel(category.id)"
+            >
+              ✕
+            </span>
           </td>
           <td class="d-flex justify-content-between">
-            <button type="button" class="btn btn-link mr-2">
+            <button
+              v-show="!category.isEditing"
+              type="button"
+              class="btn btn-link mr-2"
+              @click.stop.prevent="toggleIsEditing(category.id)"
+            >
               Edit
+            </button>
+            <button
+              v-show="category.isEditing"
+              type="button"
+              class="btn btn-link mr-2"
+              @click.stop.prevent="
+                updateCategory({ categoryId: category.id, name: category.name })
+              "
+            >
+              Save
             </button>
             <button
               type="button"
@@ -118,6 +146,10 @@ export default {
     // 4. 定義 `fetchCategories` 方法，把 `dummyData` 帶入 Vue 物件
     fetchCategories() {
       this.categories = dummyData.categories;
+      this.categories = dummyData.categories.map(category => ({
+        ...category,
+        isEditing: false
+      }));
     },
     createCategory() {
       // TODO: 透過api告知伺服器欲新增的餐廳類別
@@ -137,7 +169,63 @@ export default {
       this.categories = this.categories.filter(
         category => category.id !== categoryId
       );
+    },
+    toggleIsEditing(categoryId) {
+      this.categories = this.categories.map(category => {
+        if (category.id !== categoryId) return category;
+        // 如果迴圈中的 category.id 是欲修改的 categoryId 則改變 isEditing 的值
+        return {
+          ...category,
+          nameCached: category.name,
+          isEditing: !category.isEditing
+        };
+      });
+    },
+    updateCategory({ categoryId, name }) {
+      console.log(name);
+      // TODO: 透過 API 去向伺服器更新餐廳類別名稱
+      this.toggleIsEditing(categoryId);
+    },
+    handleCancel(categoryId) {
+      this.categories = this.categories.map(category => {
+        if (category.id !== categoryId) return category;
+        return {
+          ...category,
+          name: category.nameCached,
+          isEditing: !category.isEditing
+        };
+      });
     }
   }
 };
 </script>
+
+<style scoped>
+.category-name {
+  padding: 0.375rem 0.75rem;
+  border: 1px solid transparent;
+  outline: 0;
+  cursor: auto;
+}
+
+.btn-link {
+  width: 62px;
+}
+
+.cancel {
+  position: absolute;
+  right: 20px;
+  top: 50%;
+  transform: translateY(-50%);
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 25px;
+  height: 25px;
+  border: 1px solid #aaaaaa;
+  border-radius: 50%;
+  user-select: none;
+  cursor: pointer;
+  font-size: 12px;
+}
+</style>
