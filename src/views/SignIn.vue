04dad6a1deea183686f95a7586665075703e4c1e
Sean <siang720@gmail.com>
2020-03-01 09:22:15 +0800

    add signin feature

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -54,6 +54,8 @@
 </template>
 
 <script>
+import authorizationAPI from "../apis/authorization";
+
 export default {
   name: "SignIn",
   data() {
@@ -64,13 +66,20 @@ export default {
   },
   methods: {
     handleSubmit() {
-      const data = JSON.stringify({
-        email: this.email,
-        password: this.password
-      });
+      authorizationAPI
+        .signIn({
+          email: this.email,
+          password: this.password
+        })
+        .then(response => {
+          // 取得 API 請求後的資料
+          const { data } = response;
+          // 將 token 存放在 localStorage 內
+          localStorage.setItem("token", data.token);
 
-      // TODO: 向後端驗證使用者登入資訊是否合法
-      console.log("data", data);
+          // 成功登入後轉址到餐聽首頁
+          this.$router.push("/restaurants");
+        });
     }
   }
 };
