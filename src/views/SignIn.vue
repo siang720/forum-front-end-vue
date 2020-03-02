9b6357487364d5d8684736641b292dbbb7ad7ae4
Sean <siang720@gmail.com>
2020-03-02 21:44:36 +0800

    change signin code from promise to async/await

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -71,45 +71,42 @@ export default {
     };
   },
   methods: {
-    handleSubmit() {
-      // 如果 email 或 password 為空，則使用 Toast 提示
-      // 然後 return 不繼續往後執行
-      if (!this.email || !this.password) {
-        Toast.fire({
-          type: "warning",
-          title: "請填入 email 和 password"
-        });
-        return;
-      }
-
-      this.isProcessing = true;
-
-      authorizationAPI
-        .signIn({
-          email: this.email,
-          password: this.password
-        })
-        .then(response => {
-          // 取得 API 請求後的資料
-          const { data } = response;
-          // 將 token 存放在 localStorage 內
-          localStorage.setItem("token", data.token);
-
-          // 成功登入後轉址到餐聽首頁
-          this.$router.push("/restaurants");
-        })
-        .catch(error => {
-          // 將密碼欄位清空
-          this.password = "";
-
-          // 顯示錯誤提示
+    async handleSubmit() {
+      try {
+        // 如果 email 或 password 為空，則使用 Toast 提示
+        // 然後 return 不繼續往後執行
+        if (!this.email || !this.password) {
           Toast.fire({
             type: "warning",
-            title: "您輸入的帳號密碼錯誤"
+            title: "請填入 email 和 password"
           });
-          this.isProcessing = false;
-          console.log("error", error);
+          return;
+        }
+
+        this.isProcessing = true;
+
+        const response = await authorizationAPI.signIn({
+          email: this.email,
+          password: this.password
         });
+        // 取得 API 請求後的資料
+        const { data } = response;
+        // 將 token 存放在 localStorage 內
+        localStorage.setItem("token", data.token);
+
+        // 成功登入後轉址到餐聽首頁
+        this.$router.push("/restaurants");
+      } catch (e) {
+        // 將密碼欄位清空
+        this.password = "";
+
+        // 顯示錯誤提示
+        Toast.fire({
+          icon: "warning",
+          title: "您輸入的帳號密碼錯誤"
+        });
+        this.isProcessing = false;
+      }
     }
   }
 };
