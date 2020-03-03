497517c04b35ade0f2d744836f39bcff269f48e8
Sean <siang720@gmail.com>
2020-03-04 00:59:18 +0800

    GET admin/restaurant/:id

--- src/views/AdminRestaurant.vue
+++ src/views/AdminRestaurant.vue
@@ -38,27 +38,9 @@
 </template>
 <script>
 import { emptyImageFilter } from "./../utils/mixins";
-const dummyData = {
-  restaurant: {
-    id: 2,
-    name: "Mrs. Mckenzie Johnston",
-    tel: "567-714-6131 x621",
-    address: "61371 Rosalinda Knoll",
-    opening_hours: "08:00",
-    description:
-      "Quia pariatur perferendis architecto tenetur omnis pariatur tempore.",
-    image: "https://loremflickr.com/320/240/food,dessert,restaurant/?random=2",
-    createdAt: "2019-06-22T09:00:43.000Z",
-    updatedAt: "2019-06-22T09:00:43.000Z",
-    CategoryId: 3,
-    Category: {
-      id: 3,
-      name: "義大利料理",
-      createdAt: "2019-06-22T09:00:43.000Z",
-      updatedAt: "2019-06-22T09:00:43.000Z"
-    }
-  }
-};
+import adminAPI from "./../apis/admin";
+import { Toast } from "./../utils/helpers";
+
 export default {
   name: "AdminRestaurant",
   mixins: [emptyImageFilter],
@@ -72,29 +54,52 @@ export default {
         openingHours: "",
         tel: "",
         address: "",
-        description: ""
+        description: "",
+        viewCounts: 0,
+        Category: {}
       }
     };
   },
-  mounted() {
+  created() {
     const { id: restaurantId } = this.$route.params;
     this.fetchRestaurant(restaurantId);
   },
+  beforeRouteUpdate(to, from, next) {
+    // 路由改變時重新抓取資料
+    const { id } = to.params;
+    this.fetchRestaurant(id);
+    next();
+  },
   methods: {
-    fetchRestaurant(restaurantId) {
-      console.log(restaurantId);
-      const { restaurant } = dummyData;
-      this.restaurant = {
-        ...this.restaurant,
-        id: restaurant.id,
-        name: restaurant.name,
-        categoryName: restaurant.Category.name,
-        image: restaurant.image,
-        openingHours: restaurant.opening_hours,
-        tel: restaurant.tel,
-        address: restaurant.address,
-        description: restaurant.description
-      };
+    async fetchRestaurant(restaurantId) {
+      try {
+        const {
+          data: { restaurant },
+          statusText
+        } = await adminAPI.restaurants.getDetail({ restaurantId });
+
+        if (statusText !== "OK") {
+          throw new Error(statusText);
+        }
+
+        // STEP 3: 將資料帶入 Vue 內
+        this.restaurant = {
+          ...this.restaurant,
+          id: restaurant.id,
+          name: restaurant.name,
+          tel: restaurant.tel,
+          address: restaurant.address,
+          description: restaurant.description,
+          image: restaurant.image,
+          openingHours: restaurant.opening_hours,
+          categoryName: restaurant.Category.name
+        };
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法取得餐廳資料，請稍後再試"
+        });
+      }
     }
   }
 };
