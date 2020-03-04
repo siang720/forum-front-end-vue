535eb1cea1454c3f7e05089d7a063911c3668221
Sean <siang720@gmail.com>
2020-03-04 23:44:45 +0800

    use vuex to identify login user

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import NotFound from "../views/NotFound.vue";
 import SignIn from "../views/SignIn.vue";
 import Restaurants from "../views/Restaurants.vue";
+import store from "../store/index";
 
 Vue.use(VueRouter);
 
@@ -102,6 +103,12 @@ const routes = [
 const router = new VueRouter({
   linkExactActiveClass: "active",
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  // 使用 dispatch 呼叫 Vuex 內的 actions
+  store.dispatch("fetchCurrentUser");
+  next();
 });
 
 export default router;
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,54 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import usersAPI from "../apis/users";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    currentUser: {
+      id: -1,
+      name: "",
+      email: "",
+      image: "",
+      isAdmin: false
+    },
+    isAuthenticated: false
   },
   mutations: {
+    setCurrentUser(state, currentUser) {
+      state.currentUser = {
+        ...state.currentUser,
+        // 將 API 取得的 currentUser 覆蓋掉 Vuex state 中的 currentUser
+        ...currentUser
+      };
+      // 將使用者的登入狀態改為 true
+      state.isAuthenticated = true;
+    }
   },
   actions: {
+    // 在 actions 中可以透過參數的方式取得 commit 的方法
+    async fetchCurrentUser({ commit }) {
+      try {
+        const { data, statusText } = await usersAPI.getCurrentUser();
+
+        if (statusText !== "OK") {
+          throw new Error(statusText);
+        }
+
+        commit("setCurrentUser", {
+          id: data.id,
+          name: data.name,
+          email: data.email,
+          image: data.image,
+          isAdmin: data.isAdmin
+        });
+        return true;
+      } catch (error) {
+        commit("revokeAuthentication");
+        return false;
+      }
+    }
   },
-  modules: {
-  }
-})
+  modules: {}
+});
